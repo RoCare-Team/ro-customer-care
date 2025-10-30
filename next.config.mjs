f5849f9fc707a843543197afc27b86be93b79963
webDevSociapa <webdev@sociapa.com>
2025-10-30 14:12:57 +0530

    fixess

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   images: {
     domains: ["www.waterpurifierservicecenter.in"], // ✅ allow this domain
   },
+
+  async redirects() {
+    return [
+      {
+        source: "/:path*.php",
+        destination: "/:path*",
+        permanent: true, // ✅ SEO friendly redirect (301)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
